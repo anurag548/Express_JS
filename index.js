3e99d2fb32fe7422b28d93af1447215d13dc1951
Pravesh <pravesh2655@gmail.com>
2022-08-04 22:25:59 +0530

    added vercel and post api

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ require("dotenv").config(); // environment variable
 // require packages
 const express = require("express");
 const mongoose = require("mongoose");
-
+const bodyParser = require('body-parser')
 // initialise express
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
 
+// parse application/json
+app.use(bodyParser.json())
 //  mondodb connect
 mongoose.connect(
     "mongodb+srv://Pravesh:Pravesh%402655@razor.5jfiohz.mongodb.net/?retryWrites=true&w=majority",
@@ -23,8 +26,15 @@ const studentSchema = new mongoose.Schema({
     subjects: [String]
 });
 
+const usersSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    gender: String
+})
+
 // create a model with studentSchema
 const Student = mongoose.model('Student', studentSchema);
+const Users = mongoose.model('users', usersSchema);
 
 // Create a new document
 const stud = new Student({
@@ -34,16 +44,43 @@ const stud = new Student({
     subjects: ['DBMS', 'OS', 'Graph Theory', 'Internet Programming']
 });
 // Add the document to Collections
-stud.save().then(() => console.log("One entry added"), (err) => console.log(err));
-// Save method can also be written as:
-// stud.save((err, result) => {
-//     if(err) console.log(err);
-//     else console.log("entry added");
-// });
+// stud.save().then(() => console.log("One entry added"), (err) => console.log(err));
+
+
+
 
 // get documents
 app.get('/', (req, res) => {
     Student.find({}, (err, found) => {
+        if (!err) {
+            res.send(found);
+        } else {
+            console.log(err);
+            res.send("Some error occured!")
+        }
+    }).catch(err => console.log("Error occured, " + err));
+});
+
+app.post('/adduser', (req, res) => {
+    const { name, age, gender } = req.body;
+    const details = new Users({
+        name: name,
+        age: age,
+        gender: gender
+    });
+    details.save().then(() => Users.find({}, (err, found) => {
+        if (!err) {
+            res.send(found);
+        } else {
+            console.log(err);
+            res.send("Some error occured!")
+        }
+    }).catch(err => console.log("Error occured, " + err)));
+})
+
+
+app.get('/users', (req, res) => {
+    Users.find({}, (err, found) => {
         if (!err) {
             res.send(found);
         } else {
